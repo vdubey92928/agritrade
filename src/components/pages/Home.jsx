3c488c7324e97a6a4005825162f673451a49fb24
Vivekanand Dubey <vd123754@gmail.com>
2025-08-11 10:34:10 +0530

    combining pages

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../layout/Header";
 import Footer from "../layout/Footer"
-import "../css/style.css"; // custom styles
+import "../css/home.css"; // custom styles
 
 export default function Home() {
   return (
